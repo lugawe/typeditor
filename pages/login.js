7567b379276a548b6684cb00b2a851affd375488
Luis Weich <luis.weich@study.thws.de>
2025-07-28 02:42:08 +0200

    update login.js to use states and api

--- pages/login.js
+++ pages/login.js
@@ -1,32 +1,43 @@
 // pages/login.js
-import React from 'react';
-import styles from '../styles/login.module.css';
+import styles from "../styles/login.module.css";
+import { useState } from "react";
+import { generateAccessToken } from "@/lib/api/account";
 
 export default function Login() {
+  // ...
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+
+  async function submit() {
+    const accessToken = await generateAccessToken(name, password);
+  }
+
   return (
     <div className={styles.wrapper}>
       <div className={styles.card}>
         <div className={styles.leftPanel}>
           <h1 className={styles.logo}>Typeditor</h1>
-          <p className={styles.tagline}>
-            Typst at your Fingertips!
-          </p>
-          <p className={styles.tagline}>
-            - Anyone, Anywhere, Anything!
-          </p>
+          <p className={styles.tagline}>Typst at your Fingertips!</p>
+          <p className={styles.tagline}>- Anyone, Anywhere, Anything!</p>
         </div>
         <div className={styles.rightPanel}>
           <input
             className={styles.input}
+            value={name}
+            onChange={(e) => setName(e.target.value)}
             type="text"
-            placeholder="ID"
+            placeholder="Name"
           />
           <input
             className={styles.input}
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
             type="password"
             placeholder="Password"
           />
-          <button className={styles.button}>Sign in</button>
+          <button className={styles.button} onClick={submit}>
+            Sign in
+          </button>
           <a className={styles.link} href="/signup">
             Sign up
           </a>
