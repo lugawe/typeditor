5aba3f56299cbac1354b321c206db192c5739a98
Luis Weich <luis.weich@study.thws.de>
2025-07-28 14:27:20 +0200

    use forms

--- pages/signup.js
+++ pages/signup.js
@@ -1,38 +1,64 @@
-import React from 'react';
-import styles from '../styles/login.module.css';
+// pages/signup.js
+import styles from "@/styles/login.module.css";
+import { useRouter } from "next/router";
+import { useState } from "react";
+import { createAccount } from "@/lib/api/account";
 
 export default function Signup() {
+  // ...
+  const router = useRouter();
+
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  async function submit(e) {
+    e.preventDefault();
+    // const account = await createAccount(name, password);
+    await router.push("/projects_overview");
+  }
+
   return (
     <div className={styles.wrapper}>
       <div className={styles.card}>
         <div className={styles.leftPanel}>
           <h1 className={styles.logo}>Typeditor</h1>
           <p className={styles.tagline}>
-            Start building beautiful docs
-            in seconds
+            Start building beautiful docs in seconds
           </p>
         </div>
-        <div className={styles.rightPanel}>
+        <form className={styles.rightPanel} onSubmit={submit}>
           <input
             className={styles.input}
+            value={name}
+            onChange={(e) => setName(e.target.value)}
             type="text"
-            placeholder="ID"
+            placeholder="Name"
+            required
           />
           <input
             className={styles.input}
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
             type="password"
             placeholder="Password"
+            required
           />
           <input
             className={styles.input}
+            value={confirmPassword}
+            onChange={(e) => setConfirmPassword(e.target.value)}
             type="password"
             placeholder="Confirm password"
+            required
           />
-          <button className={styles.button}>Sign up</button>
+          <button className={styles.button} type="submit">
+            Sign up
+          </button>
           <a className={styles.link} href="login">
             Login
           </a>
-        </div>
+        </form>
       </div>
     </div>
   );
