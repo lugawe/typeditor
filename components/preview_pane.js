e2033300331e5fd67015825ecbbb729383d6849b
Honghee <honghee.kim@study.thws.de>
2025-07-30 00:45:34 +0200

    update PDF viewer

--- components/preview_pane.js
+++ components/preview_pane.js
@@ -1,15 +1,30 @@
 import React from 'react';
 import styles from '../styles/editor.module.css';
 
+export function ExportButton({ onClick }) {
+  return (
+    <button
+      onClick={onClick}
+      style={{
+        display: 'flex',
+        alignItems: 'center',
+        cursor: 'pointer',
+      }}
+    >
+      <img src="/Export.svg" alt="Export Icon" style={{ width: 55, height: 29, objectFit: 'contain' }} />
+    </button>
+  );
+}
+
 export default function PreviewPane({ pdfUrl }) {
     return (
         <div className={styles.previewPane}>
             <div className={styles.examplepdf}>
-                <iframe
+                 <iframe
                     src={pdfUrl}
                     width="100%"
                     height="100%"
-                    style={{ border: "none" }}
+                    style={{ border: "none", flex: 1 }}
                     title="PDF Preview"
                 />
             </div>
